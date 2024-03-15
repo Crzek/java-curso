c3b9f2876dcbbec0fe61e1ffd72159c6e21830e3
cruz-ubunto <javier1996.ec@gmail.com>
2024-03-15 01:04:02 +0100

    model/Invoice

---
+++ spring-web-modules/spring-mvc-basics/src/main/java/com/baeldung/model/Invoice.java
@@ -0,0 +1,68 @@
+package com.baeldung.model;
+
+import java.util.Date;
+
+public class Invoice {
+    private int id;
+    private String concept;
+
+    private long value;
+    private float percent;
+    private Date dateInvoice;
+
+    public Invoice(String concept, long value,float percent,Date dateInvoice){
+        this.concept = concept;
+        this.value =value;
+        this.percent = percent;
+        this.dateInvoice = dateInvoice;
+    }
+
+    @Override
+    public String toString() {
+        return "Invoice{" +
+                "concept='" + concept + '\'' +
+                ", value=" + value +
+                ", percent=" + percent +
+                ", dateInvoice=" + dateInvoice +
+                '}';
+    }
+
+    public String getConcept() {
+        return concept;
+    }
+
+    public void setConcept(String concept) {
+        this.concept = concept;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public long getValue() {
+        return value;
+    }
+
+    public void setValue(long value) {
+        this.value = value;
+    }
+
+    public float getPercent() {
+        return percent;
+    }
+
+    public void setPercent(float percent) {
+        this.percent = percent;
+    }
+
+    public Date getDateInvoice() {
+        return dateInvoice;
+    }
+
+    public void setDateInvoice(Date dateInvoice) {
+        this.dateInvoice = dateInvoice;
+    }
+}
---
+++ spring-web-modules/spring-mvc-basics/src/main/java/com/baeldung/web/controller/InvoiceController.java
@@ -0,0 +1,9 @@
+package com.baeldung.web.controller;
+
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class InvoiceController {
+
+}
